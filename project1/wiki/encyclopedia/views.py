0df8a1233c7bf9d151fae0fbc3e70cd9573212b1
jinook929 <jinook929@gmail.com>
2020-11-23 21:57:27 -0500

    add entry.html and error.html

--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -1,10 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils.html import format_html
+import markdown2
 
 from . import util
 
 
 def index(request):
+    entries = util.list_entries()
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": entries
     })
 
+def error(request):
+    return render(request, "encyclopedia/error.html")
+
+def entry(request, title):
+    entry = util.get_entry(title)
+    if not entry:
+        return render(request, "encyclopedia/error.html", {
+            "title": title
+        })
+
+    return render(request, "encyclopedia/entry.html", {
+        "entry": format_html(markdown2.markdown(entry))
+    })
