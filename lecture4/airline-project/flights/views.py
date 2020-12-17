5095555a2b50ff27bdbb0eae555244285da281e6
jinook929 <jinook929@gmail.com>
2020-12-16 22:24:30 -0500

    create django app airline

---
+++ lecture4/airline-project/flights/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from .models import *
+
+# Create your views here.
+def index(request):
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all(),
+        "airports": Airport.objects.all(),
+    })
